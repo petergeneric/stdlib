4a799a7980ba06244c2ccfd5b30d662177da82e4
Peter <peter@peterphi.com>
2013-08-29 18:36:22 +0100

    Allow the core REST services to be disabled if required

--- guice/webapp/src/main/java/com/mediasmiths/std/guice/web/rest/setup/AbstractRESTGuiceSetup.java
+++ guice/webapp/src/main/java/com/mediasmiths/std/guice/web/rest/setup/AbstractRESTGuiceSetup.java
@@ -2,10 +2,10 @@ package com.mediasmiths.std.guice.web.rest.setup;
 
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.mediasmiths.std.indexservice.rest.client.guice.IndexServiceModule;
 import com.mediasmiths.std.guice.apploader.GuiceSetup;
 import com.mediasmiths.std.guice.web.rest.CoreRestServicesModule;
 import com.mediasmiths.std.guice.web.rest.scoping.ServletScopingModule;
+import com.mediasmiths.std.indexservice.rest.client.guice.IndexServiceModule;
 import com.mediasmiths.std.io.PropertyFile;
 import org.apache.log4j.Logger;
 
@@ -25,11 +25,24 @@ public abstract class AbstractRESTGuiceSetup implements GuiceSetup
 	 */
 	public static final String DISABLE_INDEX_SERVICE = "framework.indexservice.disabled";
 
+	/**
+	 * The override for the core REST services
+	 */
+	public static final String DISABLE_CORE_SERVICES = "framework.restcoreservices.disabled";
+
+
 	@Override
 	public void registerModules(List<Module> modules, PropertyFile config)
 	{
 		modules.add(new ServletScopingModule());
-		modules.add(new CoreRestServicesModule());
+
+		if (!config.getBoolean(DISABLE_CORE_SERVICES, false))
+		{
+			modules.add(new CoreRestServicesModule());
+		}
+		else {
+			log.info("REST Core Services disabled by config parameter");
+		}
 
 		// Enable the index service if the webapp wants to use it
 		final boolean indexServiceDisabled = config.getBoolean(DISABLE_INDEX_SERVICE, false);
@@ -49,13 +62,16 @@ public abstract class AbstractRESTGuiceSetup implements GuiceSetup
 		addModules(modules, config);
 	}
 
+
 	public abstract void addModules(List<Module> modules, PropertyFile config);
+
 
 	@Override
 	public final void injectorCreated(Injector injector)
 	{
 		injectorWasCreated(injector);
 	}
+
 
 	public abstract void injectorWasCreated(Injector injector);
 }
