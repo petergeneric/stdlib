72dc45215e2af78beb5ad1755566a73040f501a4
Peter <peter@peterphi.com>
2014-02-22 17:52:39 +0000

    Missed java.util.Properties import, caused compile to fail

--- stdlib/src/main/java/com/peterphi/std/io/PropertyFile.java
+++ stdlib/src/main/java/com/peterphi/std/io/PropertyFile.java
@@ -35,6 +35,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 public class PropertyFile
