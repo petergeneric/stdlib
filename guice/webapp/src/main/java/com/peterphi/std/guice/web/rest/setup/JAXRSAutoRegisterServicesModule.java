30884d18b92779d9e119de3cc61451ecd9b76bdb
Peter <peter@peterphi.com>
2014-08-21 17:13:45 +0100

    Allow auto-registering of JAX-RS resources that are classes, not just interfaces

--- guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/setup/JAXRSAutoRegisterServicesModule.java
+++ guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/setup/JAXRSAutoRegisterServicesModule.java
@@ -11,12 +11,15 @@ import javax.ws.rs.Path;
 import java.util.List;
 
 /**
- * Discovers all interfaces annotated with {@link javax.ws.rs.Path} and registers them with the {@link
+ * Discovers all classes/interfaces annotated with {@link javax.ws.rs.Path} and registers them with the {@link
  * com.peterphi.std.guice.serviceregistry.rest.RestResource}.<br />
  * <p/>
  * In addition, searches for implementations (unless the interface is annotated with {@link com.google.inject.ImplementedBy}) and
  * auto-binds a REST interface to an implementation. If there is more than one implementation then auto-binding will fail and
  * startup will not be able to proceed.
+ * <p/>
+ * If there are no implementations found then it is assumed the interface is a client interface in this context and it is
+ * ignored.
  */
 class JAXRSAutoRegisterServicesModule extends AbstractModule
 {
@@ -34,8 +37,8 @@ class JAXRSAutoRegisterServicesModule extends AbstractModule
 	@Override
 	protected void configure()
 	{
-		// scan for @Path annotated interfaces
-		for (Class<?> iface : scanner.getAnnotatedClasses(Path.class, ClassScanner.interfaceClass()))
+		// scan for @Path annotated classes
+		for (Class<?> iface : scanner.getAnnotatedClasses(Path.class))
 		{
 			try
 			{
@@ -49,35 +52,36 @@ class JAXRSAutoRegisterServicesModule extends AbstractModule
 	}
 
 
-	private <T> void autobind(Class<T> iface)
+	private <T> void autobind(Class<T> clazz)
 	{
-		if (iface.isAnnotationPresent(ImplementedBy.class))
+		// If the class is a regular class (or an interface annotated with @ImplementedBy)...
+		if (!clazz.isInterface() || clazz.isAnnotationPresent(ImplementedBy.class))
 		{
-			// Guice knows how to bind this already so no need to search for an implementation
-			RestResourceRegistry.register(iface);
+			// Guice knows how to bind this already so no need to search for an implementation, just expose it via JAX-RS
+			RestResourceRegistry.register(clazz);
 		}
 		else
 		{
 			// Search for implementations
-			final List<Class<? extends T>> implementations = scanner.getExtendingClasses(iface);
+			final List<Class<? extends T>> implementations = scanner.getExtendingClasses(clazz);
 
 			if (implementations.size() == 1)
 			{
-				RestResourceRegistry.register(iface);
+				RestResourceRegistry.register(clazz);
 
-				bind(iface).to(implementations.get(0));
+				bind(clazz).to(implementations.get(0));
 			}
 			else if (implementations.size() == 0)
 			{
 				// TODO bind a client provider if an endpoint is available in the config?
-				log.debug("Found JAX-RS interface with no implementation. Assuming it is a client interface: " + iface);
+				log.debug("Found JAX-RS interface with no implementation. Assuming it is a client interface: " + clazz);
 				return;
 			}
 			else
 			{
 				// Too many interfaces
 				throw new IllegalArgumentException("Expected 0 or 1 implementation for auto-discovered REST interface " +
-				                                   iface +
+				                                   clazz +
 				                                   " but found " +
 				                                   implementations + ". Auto-binding REST interfaces is not possible.");
 			}
