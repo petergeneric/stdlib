2ad0869cca67d9d95f5883ee1f0e68f5d612e01f
Peter <peter@peterphi.com>
2014-09-30 10:50:10 +0100

    Don't auto-register @MappedSuperclass-annotated classes with hibernate

--- guice/hibernate/src/main/java/com/peterphi/std/guice/hibernate/role/AutoHibernateModule.java
+++ guice/hibernate/src/main/java/com/peterphi/std/guice/hibernate/role/AutoHibernateModule.java
@@ -3,12 +3,16 @@ package com.peterphi.std.guice.hibernate.role;
 import com.codahale.metrics.MetricRegistry;
 import com.peterphi.std.guice.common.ClassScanner;
 import com.peterphi.std.guice.hibernate.module.HibernateModule;
+import org.apache.log4j.Logger;
 import org.hibernate.cfg.Configuration;
 
 import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 
 class AutoHibernateModule extends HibernateModule
 {
+	private static final Logger log = Logger.getLogger(AutoHibernateModule.class);
+
 	private final ClassScanner scanner;
 
 
@@ -24,6 +28,14 @@ class AutoHibernateModule extends HibernateModule
 	protected void configure(final Configuration config)
 	{
 		for (Class<?> clazz : scanner.getAnnotatedClasses(Entity.class))
-			config.addAnnotatedClass(clazz);
+			if (!clazz.isAnnotationPresent(MappedSuperclass.class))
+			{
+				log.trace("Registering @Entity class with hibernate: " + clazz.getName());
+				config.addAnnotatedClass(clazz);
+			}
+			else
+			{
+				log.debug("Ignoring @Entity class because it has @MappedSuperclass: " + clazz.getName());
+			}
 	}
 }
