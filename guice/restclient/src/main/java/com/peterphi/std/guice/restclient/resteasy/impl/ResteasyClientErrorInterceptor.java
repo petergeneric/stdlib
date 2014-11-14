794770f72137b080ee68df199b4642b6648ff40a
Peter <peter@peterphi.com>
2014-11-14 12:09:35 +0000

    Add support for parsing exception XMLs with the legacy namespace from the mediasmiths fork of stdlib to make it easier to migrate services to mainline

--- guice/restclient/src/main/java/com/peterphi/std/guice/restclient/resteasy/impl/ResteasyClientErrorInterceptor.java
+++ guice/restclient/src/main/java/com/peterphi/std/guice/restclient/resteasy/impl/ResteasyClientErrorInterceptor.java
@@ -2,14 +2,21 @@ package com.peterphi.std.guice.restclient.resteasy.impl;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.peterphi.std.annotation.Doc;
+import com.peterphi.std.guice.common.serviceprops.annotations.Reconfigurable;
 import com.peterphi.std.guice.restclient.exception.RestException;
 import com.peterphi.std.guice.restclient.exception.RestExceptionFactory;
 import com.peterphi.std.guice.restclient.exception.RestThrowableConstants;
 import com.peterphi.std.guice.restclient.jaxb.RestFailure;
+import com.peterphi.std.util.jaxb.JAXBSerialiser;
+import com.peterphi.std.util.jaxb.JAXBSerialiserFactory;
+import org.apache.log4j.Logger;
 import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.client.ClientResponseFailure;
 import org.jboss.resteasy.client.core.BaseClientResponse;
 import org.jboss.resteasy.client.core.ClientErrorInterceptor;
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBUnmarshalException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,13 +24,20 @@ import java.io.InputStream;
 @Singleton
 public class ResteasyClientErrorInterceptor implements ClientErrorInterceptor
 {
-	private final RestExceptionFactory exceptionFactory;
+	private static final Logger log = Logger.getLogger(ResteasyClientErrorInterceptor.class);
 
 	@Inject
-	public ResteasyClientErrorInterceptor(RestExceptionFactory exceptionFactory)
-	{
-		this.exceptionFactory = exceptionFactory;
-	}
+	RestExceptionFactory exceptionFactory;
+
+	@Inject
+	JAXBSerialiserFactory serialiserFactory;
+
+	@Inject(optional = true)
+	@Reconfigurable
+	@Named("jaxrs.resteasy.try-parse-legacy-exception-namespace")
+	@Doc("If true then JAX-RS client calls producing rich exceptions will try to parse using the legacy XML namespace if unmarshalling fails for the current namespace (default true)")
+	boolean tryParseLegacyExceptionNamespace = true;
+
 
 	@Override
 	public void handle(ClientResponse<?> response) throws RuntimeException
@@ -34,13 +48,42 @@ public class ResteasyClientErrorInterceptor implements ClientErrorInterceptor
 			{
 				final BaseClientResponse<?> r = (BaseClientResponse<?>) response;
 				final InputStream stream = r.getStreamFactory().getInputStream();
-				stream.reset();
 
-				final RestFailure failure = response.getEntity(RestFailure.class);
+				RestFailure failure;
 
-				// Reset the stack trace so we don't confuse matters with the frames from the factory
+				if (tryParseLegacyExceptionNamespace)
+				{
+					// Try to parse the response. If parsing fails, fall back on the legacy XML namespace and try to parse.
+					// If parsing the legacy namespace fails, throw the original parse error back to the client
+					try
+					{
+						failure = parseResponse(stream, r);
+					}
+					catch (JAXBUnmarshalException e)
+					{
+						log.trace("Error parsing rich exception response, will fall back on parsing it as a legacy exception XML",
+						          e);
+						try
+						{
+							failure = parseLegacyResponse(stream, r);
+						}
+						catch (Throwable legacyFailure)
+						{
+							log.trace("Error parsing rich exception response as legacy rich exception XML!", legacyFailure);
+
+							throw e; // throw the original exception
+						}
+					}
+				}
+				else
+				{
+					failure = parseResponse(stream, r);
+				}
+
+				// Build an exception
 				final RestException exception = exceptionFactory.build(failure, response);
 
+				// Reset the stack trace so we don't confuse matters with the frames from the factory
 				exception.fillInStackTrace();
 
 				throw exception;
@@ -51,5 +94,42 @@ public class ResteasyClientErrorInterceptor implements ClientErrorInterceptor
 				                                response);
 			}
 		}
+	}
+
+
+	private RestFailure parseResponse(InputStream stream, final BaseClientResponse<?> response) throws IOException
+	{
+		stream.reset();
+
+		return response.getEntity(RestFailure.class);
+	}
+
+
+	/**
+	 * This implementation is quite ugly (in particular the fact that XML is being processed as a string!) but it's only required
+	 * very short-term as a patch to ease communication with services that are being migrated from the legacy mediasmiths codebase
+	 * to the open source stdlib
+	 *
+	 * @param stream
+	 * @param response
+	 *
+	 * @return
+	 *
+	 * @throws IOException
+	 */
+	private RestFailure parseLegacyResponse(InputStream stream, final BaseClientResponse<?> response) throws IOException
+	{
+		stream.reset();
+
+		final String xmlAsString = response.getEntity(String.class);
+
+		final String modifiedXml = xmlAsString.replace("http://ns.mediasmithsforge.com/stdlib/rest/exception",
+		                                               "http://ns.peterphi.com/stdlib/rest/exception");
+
+		final JAXBSerialiser serialiser = serialiserFactory.getInstance(RestFailure.class);
+
+		final RestFailure failure = (RestFailure) serialiser.deserialise(modifiedXml);
+
+		return failure;
 	}
 }
