6eb7c6944616fe7855e1526d3b9212727deeba1e
Peter <peter@peterphi.com>
2014-12-05 14:46:28 +0000

    Rewrite GuiceLifecycleModule to use ProvisionListener and add in a hack to detect when the provisioning has partly failed (in which case we should not call postConstruct). I'm hoping for a cleaner solution to be available - I've reported this issue at https://github.com/google/guice/issues/884

--- guice/common/src/main/java/com/peterphi/std/guice/common/lifecycle/GuiceLifecycleModule.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/lifecycle/GuiceLifecycleModule.java
@@ -1,45 +1,78 @@
 package com.peterphi.std.guice.common.lifecycle;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Binding;
+import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
+import com.google.inject.internal.Errors;
 import com.google.inject.matcher.AbstractMatcher;
-import com.google.inject.spi.InjectionListener;
-import com.google.inject.spi.TypeEncounter;
-import com.google.inject.spi.TypeListener;
+import com.google.inject.spi.ProvisionListener;
+import org.apache.log4j.Logger;
 
+import java.lang.reflect.Field;
+
+/**
+ * Hooks into guice so that when objects implementing {@link com.peterphi.std.guice.common.lifecycle.GuiceLifecycleListener} are
+ * provisioned the {@link GuiceLifecycleListener#postConstruct()} method is called
+ */
 public class GuiceLifecycleModule extends AbstractModule
 {
-	static class ImplementsGuiceLifecycleListener extends AbstractMatcher<TypeLiteral<?>>
+	private static final Logger log = Logger.getLogger(GuiceLifecycleModule.class);
+
+	static class ImplementsGuiceLifecycleBindingListener extends AbstractMatcher<Binding<?>>
 	{
-		public boolean matches(TypeLiteral<?> tpe)
+
+		@Override
+		public boolean matches(final Binding<?> binding)
 		{
-			return GuiceLifecycleListener.class.isAssignableFrom(tpe.getRawType());
+			final Key<?> key = binding.getKey();
+			final TypeLiteral<?> typeLiteral = key.getTypeLiteral();
+
+			return GuiceLifecycleListener.class.isAssignableFrom(typeLiteral.getRawType());
 		}
-
-
-		public static final ImplementsGuiceLifecycleListener INSTANCE = new ImplementsGuiceLifecycleListener();
 	}
 
 
 	@Override
 	protected void configure()
 	{
-		TypeListener listener = new TypeListener()
+		final ProvisionListener provisionListener = new ProvisionListener()
 		{
 			@Override
-			public <I> void hear(final TypeLiteral<I> typeLiteral, TypeEncounter<I> typeEncounter)
+			public <T> void onProvision(final ProvisionInvocation<T> provision)
 			{
-				typeEncounter.register(new InjectionListener<I>()
+				final T instance = provision.provision();
+
+				final Errors errors;
+				try
 				{
-					@Override
-					public void afterInjection(Object o)
-					{
-						((GuiceLifecycleListener) o).postConstruct();
-					}
-				});
+					final Class<?> clazz = provision.getClass();
+
+					final Field field = clazz.getDeclaredField("errors");
+					field.setAccessible(true);
+					errors = (Errors) field.get(provision);
+				}
+				catch (Throwable t)
+				{
+					throw new RuntimeException("Error reflectively getting Errors object to evaluate", t);
+				}
+
+				if (errors.hasErrors())
+				{
+					log.warn("Errors prevent the lifecycle initialisation of instance " + instance);
+					// Throw an exception here to stop others from seeing this broken object
+					errors.throwProvisionExceptionIfErrorsExist();
+				}
+				else
+				{
+					// Cast the constructed object to a GuiceLifecycleListener and call the postConstruct method
+					final GuiceLifecycleListener asLifecycle = ((GuiceLifecycleListener) instance);
+
+					asLifecycle.postConstruct();
+				}
 			}
 		};
 
-		bindListener(new ImplementsGuiceLifecycleListener(), listener);
+		bindListener(new ImplementsGuiceLifecycleBindingListener(), provisionListener);
 	}
 }
