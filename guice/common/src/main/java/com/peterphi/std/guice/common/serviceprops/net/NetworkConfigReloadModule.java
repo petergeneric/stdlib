754cc91b0bbd3f0b01cb67fdf45bbb2ec0377222
Peter <peter@peterphi.com>
2016-08-08 16:32:02 +0100

    Add explicit jax-rs client provider for ConfigRestService so that scan.packages doesn't have to include ConfigRestService

--- guice/common/src/main/java/com/peterphi/std/guice/common/serviceprops/net/NetworkConfigReloadModule.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/serviceprops/net/NetworkConfigReloadModule.java
@@ -1,6 +1,10 @@
 package com.peterphi.std.guice.common.serviceprops.net;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.peterphi.std.guice.config.rest.iface.ConfigRestService;
+import com.peterphi.std.guice.restclient.JAXRSProxyClientFactory;
 
 public class NetworkConfigReloadModule extends AbstractModule
 {
@@ -8,5 +12,13 @@ public class NetworkConfigReloadModule extends AbstractModule
 	protected void configure()
 	{
 		bind(NetworkConfigReloadDaemon.class).asEagerSingleton();
+	}
+
+
+	@Provides
+	@Singleton
+	public ConfigRestService getConfigService(JAXRSProxyClientFactory factory)
+	{
+		return factory.getClient(ConfigRestService.class);
 	}
 }
