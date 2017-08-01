91819f92c3e4dca9d664f474fe968e81b2f80480
Bryce McLeod <bryce.mcleod@codory.com>
2017-08-01 12:51:48 +0100

    make metric serialiser public

--- guice/metrics/src/main/java/com/peterphi/std/guice/metrics/rest/impl/MetricSerialiser.java
+++ guice/metrics/src/main/java/com/peterphi/std/guice/metrics/rest/impl/MetricSerialiser.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 
-class MetricSerialiser
+public class MetricSerialiser
 {
 	public MetricsHistogram serialise(String name, Histogram histo)
 	{
