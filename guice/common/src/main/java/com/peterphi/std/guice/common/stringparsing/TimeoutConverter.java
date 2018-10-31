6cbf6b33cac7537a09b3d83a49b11328087da188
Peter <peter@peterphi.com>
2018-10-31 12:07:14 +0000

    Allow Timeout to be configured using an ISO-8601 Duration

--- guice/common/src/main/java/com/peterphi/std/guice/common/stringparsing/TimeoutConverter.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/stringparsing/TimeoutConverter.java
@@ -2,6 +2,8 @@ package com.peterphi.std.guice.common.stringparsing;
 
 import com.peterphi.std.threading.Timeout;
 
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,6 +26,21 @@ public class TimeoutConverter
 		{
 			return new Timeout(Long.parseLong(value), TimeUnit.MILLISECONDS);
 		}
+		else if (value.startsWith("P"))
+		{
+			// ISO-8601 duration
+			try
+			{
+				final Duration iso = Duration.parse(value);
+				final TimeUnit bestUnit = pickUnit(iso);
+
+				return new Timeout(bestUnit.convert(iso.toMillis(), TimeUnit.MILLISECONDS), bestUnit);
+			}
+			catch (Throwable t)
+			{
+				throw new IllegalArgumentException("Error parsing ISO-8601 Duration: " + value, t);
+			}
+		}
 		else
 		{
 			Matcher matcher = pattern.matcher(value);
@@ -40,6 +57,40 @@ public class TimeoutConverter
 				throw new IllegalArgumentException("Cannot parse duration: " + value);
 			}
 		}
+	}
+
+
+	/**
+	 * Identify the largest unit that can accurately represent the provided duration
+	 *
+	 * @param iso
+	 *
+	 * @return
+	 */
+	private static @NotNull
+	TimeUnit pickUnit(final @NotNull Duration iso)
+	{
+		final long millis = iso.toMillis();
+
+		// Special-case values under 1 second
+		if (millis < 1000)
+			return TimeUnit.MILLISECONDS;
+
+		final long SECOND = 1000;
+		final long MINUTE = 60 * SECOND;
+		final long HOUR = 60 * MINUTE;
+		final long DAY = 24 * HOUR;
+
+		if (millis % DAY == 0)
+			return TimeUnit.DAYS;
+		else if (millis % HOUR == 0)
+			return TimeUnit.HOURS;
+		else if (millis % MINUTE == 0)
+			return TimeUnit.MINUTES;
+		else if (millis % SECOND == 0)
+			return TimeUnit.SECONDS;
+		else
+			return TimeUnit.MILLISECONDS;
 	}
 
 
