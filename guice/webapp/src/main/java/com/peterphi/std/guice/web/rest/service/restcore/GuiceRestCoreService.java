98b3e2f4dbdbe709439b60b5bda973f78285640a
Peter <peter@peterphi.com>
2019-07-01 11:29:24 +0100

    Add missing Produces annotations on core services

--- guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/service/restcore/GuiceRestCoreService.java
+++ guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/service/restcore/GuiceRestCoreService.java
@@ -6,6 +6,7 @@ import com.peterphi.std.guice.restclient.annotations.FastFailServiceClient;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Doc("Core framework development services")
 @Path("/guice/rest-core")
@@ -15,11 +16,13 @@ public interface GuiceRestCoreService
 {
 	@Doc("Returns HTTP 200 when called")
 	@GET
+	@Produces("text/plain")
 	@Path("/ping")
 	String ping();
 
 	@Doc("Restarts the Guice environment within this webapp without restarting the webapp (or fails if this functionality has been disabled)")
 	@GET
+	@Produces("text/plain")
 	@Path("/restart")
 	String restart() throws Exception;
 }
