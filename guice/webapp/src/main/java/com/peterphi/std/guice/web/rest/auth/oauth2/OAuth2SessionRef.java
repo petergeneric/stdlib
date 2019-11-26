24b2959cd906b96aa869f4eb47eba63b6b44f49e
Peter <peter@peterphi.com>
2019-11-26 10:07:45 +0000

    This commit addresses an issue we've observed with the oauth2 flow for services, whereby a user clicking a link without having an active session with a client service can cause 2 oauth flows in parallel (may be because of double-clicking on a link) - specifically we saw 2 oauth2 flows start 40ms apart. They both returned from the oauth2 provider, but only one succeeded (because 2 sessions were created, so the browser used the most recent one). This then resulted in one link getting an error about an unexpected auth attempt, and the other session logging in and returning to the desired resource in the client service.
    This commit changes the verification of the "state" function, such that on an internal error, we ignore the nonce value check and simply follow the redirect; the reason for this is that we will (and can, due to using an HTTP 303 See Other redirect) only issue a GET redirect, we don't allow re-running any POST operation automatically. This means that malicious users can't trick users into clicking a link that will result in a modification operation, only a GET operation (which they could trigger anyway via a direct malicious link). The only change is the value of the Referer header, but that header shouldn't (and isn't) used for security purposes.
    
    Refactor OAuth2 Client Callback handling such that:
     - Scope for OAuth2 Flow includes the method (must be GET currently)
     - On an OAuth2 Client Callback processing failure, if a GET request, redirect the client browser to the redirectTo endpoint (this will restart the oauth2 auth flow if necessary, or if the user has already successfully logged in, it will simply return the desired resource).

--- guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/auth/oauth2/OAuth2SessionRef.java
+++ guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/auth/oauth2/OAuth2SessionRef.java
@@ -118,6 +118,9 @@ public class OAuth2SessionRef
 
 	public boolean isValid()
 	{
+		if (response == null)
+			return false; // Cannot be valid because there is no response data available at all
+
 		try
 		{
 			getToken();
@@ -172,16 +175,16 @@ public class OAuth2SessionRef
 			builder.replaceQueryParam("scope", scope);
 
 		if (returnTo != null)
-			builder.replaceQueryParam("state", encodeState(callbackNonce + " " + returnTo));
+			builder.replaceQueryParam("state", encodeState(callbackNonce + " GET " + returnTo));
 
 		return builder.build();
 	}
 
 
 	/**
-	 * Encode the state value into RFC $648 URL-safe base64
+	 * Encode the state value into RFC $648 URL-safe base64.
 	 *
-	 * @param src
+	 * @param src Should be of the form <code>nonce [http request] [redirect url]</code>
 	 *
 	 * @return
 	 */
@@ -192,13 +195,15 @@ public class OAuth2SessionRef
 
 
 	/**
-	 * Decode the state value (which should be encoded as RFC 4648 URL-safe base64)
+	 * Decode the state value (which should be encoded as RFC 4648 URL-safe base64)<br />
+	 * The state should be encoded as: <code>nonce [http request] [redirect url]</code>.
 	 *
 	 * @param src
 	 *
 	 * @return
+	 * @see #encodeState(String)
 	 */
-	private String decodeState(final String src)
+	private static String decodeState(final String src)
 	{
 		return new String(Base64.getUrlDecoder().decode(src));
 	}
@@ -213,16 +218,37 @@ public class OAuth2SessionRef
 	 */
 	public URI getRedirectToFromState(final String state)
 	{
-		final String[] pieces = decodeState(state).split(" ", 2);
+		final String[] pieces = decodeState(state).split(" ", 3);
 
 		if (!StringUtils.equals(callbackNonce, pieces[0]))
 			throw new IllegalArgumentException(
 					"WARNING: This service received an authorisation approval which it did not initiate, someone may be trying to compromise your account security");
 
-		if (pieces.length == 2)
-			return URI.create(pieces[1]);
+		if (pieces.length >= 2)
+		{
+			return getRedirectToFromStateIgnoringNonce(state);
+		}
 		else
+		{
 			return null;
+		}
+	}
+
+
+	/**
+	 * Gets the URI the user should GET for the redirect. N.B. fails on anything other than GET.
+	 *
+	 * @param state
+	 * @return
+	 */
+	public static URI getRedirectToFromStateIgnoringNonce(final String state)
+	{
+		final String[] pieces = decodeState(state).split(" ", 3);
+
+		if (!StringUtils.equals("GET", pieces[1]))
+			throw new IllegalArgumentException("WARNING: Unsupported redirect type (expected GET)");
+
+		return URI.create(pieces[2]);
 	}
 
 
