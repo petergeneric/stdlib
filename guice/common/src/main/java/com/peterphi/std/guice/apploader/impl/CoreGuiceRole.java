13d6dd8f266d2d5f25f512d920c9a2170f5264ea
Peter <peter@peterphi.com>
2021-02-18 17:54:19 +0000

    Only index guice properties if we're not in unit test mode (no point since unit tests won't be displaying a UI of all config props)

--- guice/common/src/main/java/com/peterphi/std/guice/apploader/impl/CoreGuiceRole.java
+++ guice/common/src/main/java/com/peterphi/std/guice/apploader/impl/CoreGuiceRole.java
@@ -4,13 +4,14 @@ import com.codahale.metrics.MetricRegistry;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Stage;
+import com.peterphi.std.guice.apploader.GuiceProperties;
 import com.peterphi.std.guice.apploader.GuiceRole;
 import com.peterphi.std.guice.apploader.GuiceSetup;
 import com.peterphi.std.guice.common.ClassScannerFactory;
 import com.peterphi.std.guice.common.JAXBModule;
-import com.peterphi.std.guice.common.logging.LoggingModule;
 import com.peterphi.std.guice.common.cached.module.CacheModule;
 import com.peterphi.std.guice.common.lifecycle.GuiceLifecycleModule;
+import com.peterphi.std.guice.common.logging.LoggingModule;
 import com.peterphi.std.guice.common.metrics.CoreMetricsModule;
 import com.peterphi.std.guice.common.retry.module.RetryModule;
 import com.peterphi.std.guice.common.serviceprops.ConfigurationPropertyRegistryModule;
@@ -44,7 +45,11 @@ class CoreGuiceRole implements GuiceRole
 	                     final MetricRegistry metrics)
 	{
 		modules.add(new ServicePropertiesModule(config));
-		modules.add(new ConfigurationPropertyRegistryModule(config, injectorRef));
+
+		// Don't gather all properties if we're in a unit test (performance)
+		if (!config.getBoolean(GuiceProperties.UNIT_TEST, false))
+			modules.add(new ConfigurationPropertyRegistryModule(config, injectorRef));
+
 		modules.add(new GuiceLifecycleModule());
 		modules.add(new CoreMetricsModule(metrics));
 		modules.add(new CacheModule(metrics));
