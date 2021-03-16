ddb908cf44df021785de48268dabd7aaa62307b9
Peter <peter@peterphi.com>
2021-03-16 15:24:52 +0000

    Update mockito-core version, and maven-compiler-plugin and maven-surefire-plugin versions

--- guice/testing/src/main/java/com/peterphi/std/guice/testing/GuiceUnit.java
+++ guice/testing/src/main/java/com/peterphi/std/guice/testing/GuiceUnit.java
@@ -6,12 +6,15 @@ import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.Statement;
-import org.mockito.internal.runners.util.FrameworkUsageValidator;
+import org.mockito.Mockito;
 
 import java.lang.annotation.Annotation;
 import java.util.List;
@@ -206,6 +209,37 @@ public class GuiceUnit extends BlockJUnit4ClassRunner
 		for (FrameworkMethod eachTestMethod : methods)
 		{
 			eachTestMethod.validatePublicVoid(isStatic, errors);
+		}
+	}
+
+
+	/**
+	 * From Mockito 1
+	 */
+	private static class FrameworkUsageValidator extends RunListener
+	{
+
+		private final RunNotifier notifier;
+
+
+		public FrameworkUsageValidator(RunNotifier notifier)
+		{
+			this.notifier = notifier;
+		}
+
+
+		@Override
+		public void testFinished(Description description) throws Exception
+		{
+			super.testFinished(description);
+			try
+			{
+				Mockito.validateMockitoUsage();
+			}
+			catch (Throwable t)
+			{
+				notifier.fireTestFailure(new Failure(description, t));
+			}
 		}
 	}
 }
