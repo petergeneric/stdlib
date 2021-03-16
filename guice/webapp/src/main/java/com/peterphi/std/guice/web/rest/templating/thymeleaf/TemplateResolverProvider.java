1332b81b4f3be127b2f6a6c65362c39e6ac58511
Peter <peter@peterphi.com>
2021-03-16 20:07:27 +0000

    Update logic to compile under thymeleaf v3

--- guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/templating/thymeleaf/TemplateResolverProvider.java
+++ guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/templating/thymeleaf/TemplateResolverProvider.java
@@ -8,6 +8,7 @@ import com.peterphi.std.threading.Timeout;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
+import javax.servlet.ServletContext;
 import java.util.concurrent.TimeUnit;
 
 public class TemplateResolverProvider implements Provider<ITemplateResolver>
@@ -17,11 +18,13 @@ public class TemplateResolverProvider implements Provider<ITemplateResolver>
 	@Doc("The maximum Time-To-Live value on the thymeleaf in-memory template cache")
 	Timeout cacheTTL = new Timeout(1, TimeUnit.HOURS);
 
+	@Inject
+	ServletContext ctx;
 
 	@Override
 	public ITemplateResolver get()
 	{
-		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(ctx);
 
 		resolver.setTemplateMode("HTML5");
 
