2edf82993dc9d24d7a5496270ad4825319b54199
Peter <peter@peterphi.com>
2021-05-20 16:13:45 +0100

    Add new optional config framework.breakers.trip-on-startup which can be set to a comma-separated value of breaker names that should be tripped by default on startup. This allows starting up a service in a limited operational mode

--- guice/common/src/main/java/com/peterphi/std/guice/common/breaker/BreakerService.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/breaker/BreakerService.java
@@ -4,10 +4,13 @@ import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.peterphi.std.guice.apploader.GuiceProperties;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
@@ -46,6 +49,9 @@ public class BreakerService
 	 */
 	private final List<BreakerGroupImpl> groups = new ArrayList<>();
 
+	@Inject(optional = true)
+	@Named(GuiceProperties.BREAKERS_TRIPPED_BY_DEFAULT)
+	public String trippedByDefault = "";
 
 	@Inject
 	public BreakerService(MetricRegistry metrics)
@@ -63,6 +69,11 @@ public class BreakerService
 	public synchronized Breaker register(Consumer<Boolean> onChange, List<String> names)
 	{
 		BreakerGroupImpl group = new BreakerGroupImpl(onChange, names.toArray(new String[names.size()]));
+
+		if (this.tripped.isEmpty() && !this.trippedByDefault.isEmpty() && !this.trippedByDefault.equalsIgnoreCase("none"))
+		{
+			tripped.addAll(Arrays.asList(trippedByDefault.split(",")));
+		}
 
 		// If some isolators are tripped,
 		if (!tripped.isEmpty())
@@ -89,7 +100,7 @@ public class BreakerService
 		if (record == null)
 		{
 			if (names.contains(name))
-				return new TripRecord(new Date(0), "Initial value", false);
+				return new TripRecord(new Date(0), "Initial value", isBreakerDefaultTripped(name));
 			else
 				return null; // breaker name not known
 		}
@@ -97,6 +108,15 @@ public class BreakerService
 		{
 			return record;
 		}
+	}
+
+
+	private boolean isBreakerDefaultTripped(final String name)
+	{
+		if (this.trippedByDefault.isEmpty() || this.trippedByDefault.equalsIgnoreCase("none"))
+			return false;
+		else
+			return StringUtils.containsIgnoreCase("," + trippedByDefault + ",", "," + name + ",");
 	}
 
 
