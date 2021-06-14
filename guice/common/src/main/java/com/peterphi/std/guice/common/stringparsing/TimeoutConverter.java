081db74fe22d63de576b3ca25144ef4d26296bb7
Peter <peter@peterphi.com>
2021-06-14 15:18:24 +0100

    Update TimeoutConverter to allow it to support ISO8601 Durations as an input, syncing up with the features of the latest version

--- guice/common/src/main/java/com/peterphi/std/guice/common/stringparsing/TimeoutConverter.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/stringparsing/TimeoutConverter.java
@@ -2,19 +2,26 @@ package com.peterphi.std.guice.common.stringparsing;
 
 import com.peterphi.std.threading.Timeout;
 
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Interprets a number of milliseconds as a Timeout
+ * Interprets a number of milliseconds as a Timeout<br /> Supports the following string representations:
+ * <ul>
+ *     <li>{@link Duration#parse(java.lang.CharSequence) ISO-8601 Duration}</li>
+ *     <li><em>###</em> - treated as milliseconds</li>
+ *     <li><em>###</em><em>(d|h|m|s|ms)</em> (days, hours, minutes, seconds, milliseconds)</li>
+ * </ul>
  */
 public class TimeoutConverter
 {
 	private static final Pattern pattern = Pattern.compile("^([0-9]+)\\s*([a-zA-Z]+)$", Pattern.CASE_INSENSITIVE);
 
 
-	public Object convert(String value)
+	public Timeout convert(String value)
 	{
 		value = value.trim();
 
@@ -23,6 +30,21 @@ public class TimeoutConverter
 		if (Character.isDigit(last))
 		{
 			return new Timeout(Long.parseLong(value), TimeUnit.MILLISECONDS);
+		}
+		else if (value.startsWith("P"))
+		{
+			// ISO-8601 duration
+			try
+			{
+				final Duration iso = Duration.parse(value);
+				final TimeUnit bestUnit = pickUnit(iso);
+
+				return new Timeout(bestUnit.convert(iso.toMillis(), TimeUnit.MILLISECONDS), bestUnit);
+			}
+			catch (Throwable t)
+			{
+				throw new IllegalArgumentException("Error parsing ISO-8601 Duration: " + value, t);
+			}
 		}
 		else
 		{
@@ -40,6 +62,40 @@ public class TimeoutConverter
 				throw new IllegalArgumentException("Cannot parse duration: " + value);
 			}
 		}
+	}
+
+
+	/**
+	 * Identify the largest unit that can accurately represent the provided duration
+	 *
+	 * @param iso
+	 *
+	 * @return
+	 */
+	private static @NotNull
+	TimeUnit pickUnit(final @NotNull Duration iso)
+	{
+		final long millis = iso.toMillis();
+
+		// Special-case values under 1 second
+		if (millis < 1000)
+			return TimeUnit.MILLISECONDS;
+
+		final long SECOND = 1000;
+		final long MINUTE = 60 * SECOND;
+		final long HOUR = 60 * MINUTE;
+		final long DAY = 24 * HOUR;
+
+		if (millis % DAY == 0)
+			return TimeUnit.DAYS;
+		else if (millis % HOUR == 0)
+			return TimeUnit.HOURS;
+		else if (millis % MINUTE == 0)
+			return TimeUnit.MINUTES;
+		else if (millis % SECOND == 0)
+			return TimeUnit.SECONDS;
+		else
+			return TimeUnit.MILLISECONDS;
 	}
 
 
