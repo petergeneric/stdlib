26feed1ff09138ff3804b9c02d2e14a7c39aec1d
Peter <peter@peterphi.com>
2021-10-19 10:40:31 +0100

    Remove defunct rules module

--- rules/src/test/java/com/peterphi/rules/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ns.peterphi.com/stdlib/rules/test",
-		                                    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.peterphi.rules;
