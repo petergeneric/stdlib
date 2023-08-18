900cdea99ad9bbbac7d65d21bed777882570b389
Peter <code@peter.works>
2023-08-18 11:57:30 +0100

    Introduce an abstract GuiceUnit test case, EmbeddedPostgresTestCase, that will override any unit testing db config to use postgresql, allowing unit tests to be tried with postgres alongside hsqldb. Currently this doesn't allow the same unit test code to be run against postgres+hsqldb, but it does allow liquibase to be run easily against both, and in the future could be extended so that all test methods will be run twice (once with hsqldb, once with postgres)

---
+++ guice/hibernate-testing/src/main/java/com/peterphi/std/guice/hibernatetest/EmbeddedPostgresTestCase.java
@@ -0,0 +1,30 @@
+package com.peterphi.std.guice.hibernatetest;
+
+import com.peterphi.std.annotation.Doc;
+import com.peterphi.std.guice.hibernatetest.pgembedded.EmbeddedPostgresDriver;
+import com.peterphi.std.guice.testing.com.peterphi.std.guice.testing.annotations.TestConfig;
+import com.peterphi.std.io.PropertyFile;
+
+public abstract class EmbeddedPostgresTestCase
+{
+	@Doc("The property name set when the database config us being overridden")
+	public static final String DATABASE_OVERRIDE_CONFIG = "database.override";
+
+	public static final String DATABASE_OVERRIDE_VAL_POSTGRES = "postgresql";
+
+
+	@TestConfig
+	public static PropertyFile getPostgresOverrideConfig()
+	{
+		PropertyFile f = new PropertyFile();
+
+		f.set("hibernate.connection.driver_class", EmbeddedPostgresDriver.class.getName());
+		f.set("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+		f.set("hibernate.connection.url", "jdbc:embedded-postgres:shared");
+
+		// Allow
+		f.set(DATABASE_OVERRIDE_CONFIG, DATABASE_OVERRIDE_VAL_POSTGRES);
+
+		return f;
+	}
+}
