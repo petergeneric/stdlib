40d046901ad0a30fae7bbcc22e4cefe628714c31
Peter <code@peter.works>
2023-09-28 01:36:13 +0100

    Update RestExceptionFactory to introduce a cache of constructor lookups

--- guice/common/src/main/java/com/peterphi/std/guice/restclient/exception/RestExceptionFactory.java
+++ guice/common/src/main/java/com/peterphi/std/guice/restclient/exception/RestExceptionFactory.java
@@ -1,18 +1,28 @@
 package com.peterphi.std.guice.restclient.exception;
 
-import com.google.inject.Singleton;
 import com.peterphi.std.guice.restclient.jaxb.RestFailure;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.ws.rs.client.ClientResponseContext;
 import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * A factory that reflectively builds instances of RestException<br />
  * This factory is used to gain access to the package-private setter methods in RestException
  */
-@Singleton
 public class RestExceptionFactory
 {
+	private static final int MAX_CACHE_SIZE = 1024;
+	private static final int MAX_CLASS_NAME_LENGTH = 1024;
+
+
+	private final Map<String, Optional<Constructor<RestException>>> cached = new HashMap<>();
+
+
 	public RestException build(final RestFailure failure, final ClientResponseContext responseContext)
 	{
 		final Constructor<RestException> constructor = getExceptionConstructor(failure);
@@ -37,34 +47,62 @@ public class RestExceptionFactory
 		return exception;
 	}
 
-	@SuppressWarnings("unchecked")
-	private Constructor<RestException> getExceptionConstructor(RestFailure failure)
-	{
-		try
-		{
-			Class<?> clazz = Class.forName(failure.exception.className, false, getClass().getClassLoader());
 
-			if (RestException.class.isAssignableFrom(clazz))
+	@SuppressWarnings("unchecked")
+	@Nullable
+	private synchronized Constructor<RestException> getExceptionConstructor(@Nonnull RestFailure failure)
+	{
+		final String className = failure.exception != null ? failure.exception.className : null;
+
+		if (className != null && className.length() < MAX_CLASS_NAME_LENGTH)
+		{
+			final Optional<Constructor<RestException>> existing = cached.get(className);
+
+			if (existing != null)
 			{
-				return (Constructor<RestException>) clazz.getConstructor(String.class, Throwable.class);
+				return existing.orElse(null);
 			}
 			else
 			{
-				return null; // not a valid exception type to rebuild
+				try
+				{
+					final Class<?> clazz = Class.forName(className, false, getClass().getClassLoader());
+
+					if (RestException.class.isAssignableFrom(clazz))
+					{
+						final Constructor<RestException> constructor = (Constructor<RestException>) clazz.getConstructor(String.class,
+						                                                                                                 Throwable.class);
+
+						if (cached.size() >= MAX_CACHE_SIZE)
+							cached.clear();
+
+						cached.put(className, Optional.of(constructor));
+
+						return constructor;
+					}
+					else
+					{
+						cached.putIfAbsent(className, Optional.empty());
+					}
+				}
+				catch (Exception | Error e)
+				{
+					// Cannot construct this exception type, don't try again in the future
+					cached.putIfAbsent(className, Optional.empty());
+					return null;
+				}
 			}
 		}
-		catch (Throwable e)
-		{
-			return null;
-		}
+
+		return null;
 	}
+
 
 	/**
 	 * Build an exception for a known exception type
 	 *
 	 * @param constructor
 	 * @param failure
-	 *
 	 * @return
 	 */
 	private RestException buildKnown(Constructor<RestException> constructor, RestFailure failure)
@@ -79,11 +117,11 @@ public class RestExceptionFactory
 		}
 	}
 
+
 	/**
 	 * Build an exception to represent an unknown or problematic exception type
 	 *
 	 * @param failure
-	 *
 	 * @return
 	 */
 	private UnboundRestException buildUnknown(RestFailure failure)
