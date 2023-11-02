d0b719077f7f03abc70845787e4c8632fe177f86
Peter <code@peter.works>
2023-11-02 14:50:25 +0000

    Update bindConfigFile so that it can supply a method to call any time config is reloaded. Update JAXBResourceProvider to synchronize around deserialise+set to ensure that in concurrent use we don't risk creating multiple short-lived copies of the config resource (N.B. this is not a correctness issue, just a GC/perf issue)

--- guice/common/src/main/java/com/peterphi/std/guice/common/serviceprops/jaxbref/JAXBResourceProvider.java
+++ guice/common/src/main/java/com/peterphi/std/guice/common/serviceprops/jaxbref/JAXBResourceProvider.java
@@ -4,26 +4,34 @@ import com.google.inject.Provider;
 import com.peterphi.std.guice.common.serviceprops.ConfigRef;
 import com.peterphi.std.threading.Timeout;
 
+import javax.annotation.Nonnull;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class JAXBResourceProvider<T> implements Provider<T>
 {
 	private final Provider<JAXBResourceFactory> resourceFactoryProvider;
 	private final Provider<ConfigRef> resource;
 	private final Class<T> clazz;
+	private final Consumer<T> onLoadMethod;
+
 	private Timeout cacheValidity = new Timeout(30, TimeUnit.SECONDS);
 
 	private volatile T value;
 	private volatile long cacheExpires = Integer.MIN_VALUE;
 
+	private final Object lock = new Object();
+
 
 	public JAXBResourceProvider(final Provider<JAXBResourceFactory> resourceFactoryProvider,
 	                            final Provider<ConfigRef> resource,
-	                            final Class<T> clazz)
+	                            final Class<T> clazz,
+	                            final Consumer<T> onLoadMethod)
 	{
 		this.resourceFactoryProvider = resourceFactoryProvider;
 		this.resource = resource;
 		this.clazz = clazz;
+		this.onLoadMethod = onLoadMethod;
 	}
 
 
@@ -32,7 +40,7 @@ public class JAXBResourceProvider<T> implements Provider<T>
 	 *
 	 * @param cacheValidity
 	 */
-	public JAXBResourceProvider withCacheValidity(final Timeout cacheValidity)
+	public JAXBResourceProvider<T> withCacheValidity(final Timeout cacheValidity)
 	{
 		this.cacheValidity = cacheValidity;
 		this.cacheExpires = Integer.MIN_VALUE;
@@ -50,23 +58,51 @@ public class JAXBResourceProvider<T> implements Provider<T>
 
 			if (obj != null)
 				return obj;
+			else
+				return compute();
 		}
-
-		final T obj = deserialise();
-
-		// If caching is enabled, cache the value
-		if (cacheValidity != null)
+		else
 		{
-			this.value = obj;
-			this.cacheExpires = System.currentTimeMillis() + cacheValidity.getMilliseconds();
+			return compute();
 		}
+	}
 
-		return obj;
+
+	/**
+	 * Synchronized to avoid multiple concurrent deserialisations (not a correctness issue, but could be a perf issue)
+	 *
+	 * @return
+	 */
+	@Nonnull
+	private synchronized T compute()
+	{
+		if (cacheExpires <= System.currentTimeMillis() || this.value == null)
+		{
+			final T obj = deserialise();
+
+			// If caching is enabled, cache the value
+			if (cacheValidity != null)
+			{
+				this.value = obj;
+				this.cacheExpires = System.currentTimeMillis() + cacheValidity.getMilliseconds();
+			}
+
+			return obj;
+		}
+		else
+		{
+			return this.value;
+		}
 	}
 
 
 	private T deserialise()
 	{
-		return resourceFactoryProvider.get().getOnce(clazz, resource.get().getName());
+		final T obj = resourceFactoryProvider.get().getOnce(clazz, resource.get().getName());
+
+		if (onLoadMethod != null)
+			onLoadMethod.accept(obj);
+
+		return obj;
 	}
 }
