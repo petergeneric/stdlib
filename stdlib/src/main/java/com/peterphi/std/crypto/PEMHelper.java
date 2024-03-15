0628f346e633ebba6a55ea7b4ff516af8aa38ac0
Peter <code@peter.works>
2024-03-15 09:54:56 +0000

    Remove bouncycastle dependency by rewriting PEMHelper to use CertificateFactory.generateCertificates instead of bouncycastle PemReader

--- stdlib/src/main/java/com/peterphi/std/crypto/PEMHelper.java
+++ stdlib/src/main/java/com/peterphi/std/crypto/PEMHelper.java
@@ -1,13 +1,8 @@
 package com.peterphi.std.crypto;
 
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
-
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -18,7 +13,7 @@ import java.security.cert.CertificateFactory;
 /**
  * PEM handling utilities
  */
-public class PEMHelper
+public final class PEMHelper
 {
 	private PEMHelper()
 	{
@@ -28,36 +23,20 @@ public class PEMHelper
 	/**
 	 * Load one or more X.509 Certificates from a PEM file
 	 *
-	 * @param pemFile
-	 * 		A PKCS8 PEM file containing only <code>CERTIFICATE</code> / <code>X.509 CERTIFICATE</code> blocks
-	 *
+	 * @param pemFile A PKCS8 PEM file containing only <code>CERTIFICATE</code> / <code>X.509 CERTIFICATE</code> blocks
 	 * @return a JKS KeyStore with the certificate aliases "cert<code>index</code>" where index is the 0-based index of the
 	 * certificate in the PEM
-	 *
-	 * @throws RuntimeException
-	 * 		if a problem occurs
+	 * @throws RuntimeException if a problem occurs
 	 */
 	public static KeyStore loadCertificates(final File pemFile)
 	{
-		try (final PemReader pem = new PemReader(new FileReader(pemFile)))
+		try (final FileInputStream pem = new FileInputStream(pemFile))
 		{
 			final KeyStore ks = createEmptyKeyStore();
 
-			int certIndex = 0;
-			Object obj;
-			while ((obj = parse(pem.readPemObject())) != null)
-			{
-				if (obj instanceof Certificate)
-				{
-					final Certificate cert = (Certificate) obj;
-
-					ks.setCertificateEntry("cert" + Integer.toString(certIndex++), cert);
-				}
-				else
-				{
-					throw new RuntimeException("Unknown PEM contents: " + obj + ". Expected a Certificate");
-				}
-			}
+			int i = 0;
+			for (Certificate cert : CertificateFactory.getInstance("X.509").generateCertificates(pem))
+				ks.setCertificateEntry("cert" + (i++), cert);
 
 			return ks;
 		}
@@ -65,50 +44,6 @@ public class PEMHelper
 		{
 			throw new RuntimeException("Error parsing PEM " + pemFile, e);
 		}
-	}
-
-
-	/**
-	 * Parse a PemObject. Currently only supports  <code>CERTIFICATE</code> / <code>X.509 CERTIFICATE</code> types
-	 *
-	 * @param obj
-	 * 		a PemObject with a type and with contents
-	 *
-	 * @return a parsed object (or null if the input is null)
-	 *
-	 * @throws GeneralSecurityException
-	 * 		if there is a parsing problem
-	 * @throws IllegalArgumentException
-	 * 		if the PemObject cannot be recognised
-	 */
-	public static Object parse(final PemObject obj) throws GeneralSecurityException
-	{
-		if (obj == null)
-		{
-			return null;
-		}
-		else if (obj.getType() == null)
-		{
-			throw new RuntimeException("Encountered invalid PemObject with null type: " + obj);
-		}
-		else if (obj.getType().equalsIgnoreCase("CERTIFICATE") || obj.getType().equalsIgnoreCase("X.509 CERTIFICATE"))
-		{
-			return parseX509Certificate(obj);
-		}
-		else
-		{
-			throw new IllegalArgumentException("Unknown PEM contents: encountered unsupported entry of type " +
-			                                   obj.getType() +
-			                                   " (expected CERTIFICATE or X.509 CERTIFICATE)");
-		}
-	}
-
-
-	private static Certificate parseX509Certificate(final PemObject obj) throws CertificateException
-	{
-		final CertificateFactory factory = CertificateFactory.getInstance("X.509");
-
-		return factory.generateCertificate(new ByteArrayInputStream(obj.getContent()));
 	}
 
 
