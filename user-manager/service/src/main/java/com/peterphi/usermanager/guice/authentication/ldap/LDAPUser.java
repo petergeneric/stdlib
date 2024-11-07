8734f4be0fdf2e1f39c311bc35e13297917b1981
Peter <code@peter.works>
2024-11-07 12:50:29 +0000

    Enforce validation on LDAP username, prohibiting special characters as defined by Microsoft for SAM-Account-Name. Also prohibits a few extra punctuation characters (unfortunately because MS defines this in terms of what is prohibited vs what is allowed, it cannot be a positive validation)

--- user-manager/service/src/main/java/com/peterphi/usermanager/guice/authentication/ldap/LDAPUser.java
+++ user-manager/service/src/main/java/com/peterphi/usermanager/guice/authentication/ldap/LDAPUser.java
@@ -3,15 +3,25 @@ package com.peterphi.usermanager.guice.authentication.ldap;
 import com.google.common.base.MoreObjects;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class LDAPUser
 {
+	/**
+	 * Matches any invalid SAM-Account-Name characters, per <a href="https://learn.microsoft.com/en-us/windows/win32/adschema/a-samaccountname">https://learn.microsoft.com/en-us/windows/win32/adschema/a-samaccountname</a> and also prohibits
+	 * <code>( ) & and whitespace characters</code>
+	 *
+	 */
+	private static final Pattern INVALID_SAM_ACCOUNT_NAME_CHARS = Pattern.compile("[\"/\\[\\]:;|=,+*?<>()&\\s]");
+
 	public final String username;
 	public final String fullyQualifiedUsername;
 
 
 	public LDAPUser(final String username, final String fullyQualifiedUsername)
 	{
+		assertValidUsername(username);
+
 		this.username = username;
 		this.fullyQualifiedUsername = fullyQualifiedUsername;
 	}
@@ -45,5 +55,11 @@ public class LDAPUser
 				       .add("username", username)
 				       .add("fullyQualifiedUsername", fullyQualifiedUsername)
 				       .toString();
+	}
+
+	private static void assertValidUsername(final String username)
+	{
+		if (INVALID_SAM_ACCOUNT_NAME_CHARS.matcher(username).matches())
+			throw new IllegalArgumentException("Unacceptable Username: " + username);
 	}
 }
