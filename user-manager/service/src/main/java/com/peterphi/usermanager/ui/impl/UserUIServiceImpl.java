f2c0294fbdff5116231d75427aba1c44fb6f8caa
Peter <code@peter.works>
2025-05-20 13:30:55 +0100

    Simplify logic working with add/remove roles sets when editing user profile

--- user-manager/service/src/main/java/com/peterphi/usermanager/ui/impl/UserUIServiceImpl.java
+++ user-manager/service/src/main/java/com/peterphi/usermanager/ui/impl/UserUIServiceImpl.java
@@ -173,28 +173,23 @@ public class UserUIServiceImpl implements UserUIService
 			delRoles.removeAll(roles);
 
 			// Add roles as necessary
-			if (addRoles.size() > 0)
+			for (String role : addRoles)
 			{
-				for (String role : addRoles)
-				{
-					RoleEntity entity = roleDao.getById(role);
-					entity.getMembers().add(user);
+				RoleEntity entity = roleDao.getById(role);
+				entity.getMembers().add(user);
 
-					roleDao.update(entity);
-				}
+				roleDao.update(entity);
 			}
 
+
 			// Remove roles as necessary
-			if (delRoles.size() > 0)
+			for (String role : delRoles)
 			{
-				for (String role : delRoles)
-				{
-					RoleEntity entity = roleDao.getById(role);
+				RoleEntity entity = roleDao.getById(role);
 
-					entity.getMembers().removeIf(u -> u.getId() == user.getId());
+				entity.getMembers().removeIf(u -> u.getId() == user.getId());
 
-					roleDao.update(entity);
-				}
+				roleDao.update(entity);
 			}
 		}
 
