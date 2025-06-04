5df5ecf2437f2f03a23aed81d6fa4871c348dacc
Peter <code@peter.works>
2025-06-04 13:53:04 +0100

    Upgrade custom postgres dialect that uses BYTEA for Lob-annotated to postgres 9.5

--- guice/hibernate/src/main/java/com/peterphi/std/guice/hibernate/dialect/PostgresDialect.java
+++ guice/hibernate/src/main/java/com/peterphi/std/guice/hibernate/dialect/PostgresDialect.java
@@ -1,13 +1,13 @@
 package com.peterphi.std.guice.hibernate.dialect;
 
-import org.hibernate.dialect.PostgreSQL82Dialect;
+import org.hibernate.dialect.PostgreSQL95Dialect;
 import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
 /**
  * A custom PostgreSQL Dialect for Hibernate that uses bytea instead of oid for byte[] Lobs
  */
-public class PostgresDialect extends PostgreSQL82Dialect
+public class PostgresDialect extends PostgreSQL95Dialect
 {
 	@Override
 	public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor)
